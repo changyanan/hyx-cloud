e2ab628c44fd3905dfae007c9c7f0d1acad22dcc
changyanan <82404677@QQ.COM>
2019-08-29 00:10:04 +0800

    搭建架子

--- hyx-config/src/main/java/org/xuenan/hyxconfig/HyxConfigApplication.java
+++ hyx-config/src/main/java/org/xuenan/hyxconfig/HyxConfigApplication.java
@@ -2,8 +2,10 @@ package org.xuenan.hyxconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class HyxConfigApplication {
 
 	public static void main(String[] args) {
