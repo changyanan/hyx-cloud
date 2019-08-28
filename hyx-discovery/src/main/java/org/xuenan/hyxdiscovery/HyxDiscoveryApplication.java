beded9c8eafb69ac8637c380a411fc0587893ad9
changyanan <changyanan@globalegrow.com>
2019-08-28 14:26:29 +0800

    test

--- hyx-discovery/src/main/java/org/xuenan/hyxdiscovery/HyxDiscoveryApplication.java
+++ hyx-discovery/src/main/java/org/xuenan/hyxdiscovery/HyxDiscoveryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HyxDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HyxDiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HyxDiscoveryApplication.class, args);
+    }
 
 }
